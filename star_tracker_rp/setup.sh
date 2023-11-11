517bce4916784eede64b37f0568e63fc40e0c304
TomCreusot <49603468+TomCreusot@users.noreply.github.com>
2023-11-11 14:00:49 +0800

    Removed Raspberry Pi
    Raspberry pi does not have enough memory to store the image

--- star_tracker_rp/setup.sh
+++
@@ -1,8 +0,0 @@
-(cd build; cmake ..)
-
-mkdir -p  ~/pico 
-(cd ~/pico/;  git clone -b master https://github.com/raspberrypi/pico-sdk.git)
-(cd ~/pico/pico-sdk; git submodule update --init)
-
-# Add SDK path to your environment
-echo 'export PICO_SDK_PATH=$HOME/pico/pico-sdk' >> ~/.bashrc
